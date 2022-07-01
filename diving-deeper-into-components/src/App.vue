b8c27c6fc2185acdd0758df22a8edc456eb5facf
R. Atasagun Poyrazoglu <rapoyrazoglu@gmail.com>
2022-07-01 22:37:54 +0300

    Section 9 Finished

--- diving-deeper-into-components/src/App.vue
+++ diving-deeper-into-components/src/App.vue
@@ -1,35 +1,44 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <!-- <BadgeList/> -->
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue'
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+import TheHeader from "./components/TheHeader.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo
+    ActiveGoals,
+    ManageGoals,
   },
-  data() { 
+  data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
---
+++ diving-deeper-into-components/src/components/BaseCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <header v-if="$slots.header">
+      <slot name="header">
+        <!-- <h3> The Default </h3>-->
+      </slot>
+    </header>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+div {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+</style>
